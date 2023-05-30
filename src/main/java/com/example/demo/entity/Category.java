b60d1a577beeec5cc062be0624014909f2b735f6
Huỳnh Quốc Trung <qtrung147@gmail.com>
2023-05-30 22:52:37 +0700

    Nop bai tuan 4

---
+++ src/main/java/com/example/demo/entity/Category.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+@Data
+@Entity
+@Table(name ="category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name ="name")
+    private String name;
+
+    @OneToMany(mappedBy ="category", cascade = CascadeType.ALL)
+    private List<Book> books;
+}
